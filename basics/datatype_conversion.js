1caf902d5f8d82e5db0fbbb8b8cee5a977e548ed
Lucky Jha <jhalucky61@gmail.com>
2024-09-20 14:54:51 +0000

    Learnt about conversion of datatypes

---
+++ basics/datatype_conversion.js
@@ -0,0 +1,27 @@
+let placement_percentage = 33;
+
+//console.log(typeof(placement_percentage)); 
+
+let score = "33%";
+//console.log(typeof(score));
+//console.log(score);
+
+// let marks = Number(score);
+// console.log(typeof(marks));
+// console.log(marks);
+
+let Number_num = Number(score);
+//console.log(typeof Number_num);
+
+// console.table([placement_percentage, Number_num, score]);
+
+// let personLoggedIn = 1; //true
+let personLoggedIn = null;
+
+let personIsLoggedIn = Boolean(personLoggedIn);
+//console.log(personIsLoggedIn);
+
+let integerName = 34566;
+let thisInteger_name = String(integerName);
+console.log(typeof(thisInteger_name));
+console.log(thisInteger_name);
--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,1 +1,9 @@
 "use strict"; // treat all js code as newer version
+
+
+//string
+//null
+//bigint
+//boolean
+//number
+//undefined
